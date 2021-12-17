3cf22a3c4bc6beb60e15fb6e67fdd6ba8d383965
Artem <a.yasenko@gmail.com>
2021-12-17 16:24:55 +0200

    Code formatted

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
-import appReducer from "./appReducer";
+import { combineReducers } from 'redux';
+import appReducer from './appReducer';
 
 export default combineReducers({ appReducer });
